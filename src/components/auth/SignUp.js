50a97966a7d06321722f758e9d1369a8cd8ed065
Usman <usman.aslam0701@gmail.com>
2020-03-06 17:10:18 +0500

    first commit

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -7,8 +7,21 @@ import firebase from '../../config/fbConfig';
 import FileUploader from 'react-firebase-file-uploader';
 import { Icon } from 'react-icons-kit';
 import { basic_lock_open } from 'react-icons-kit/linea/basic_lock_open';
+import { basic_upload } from 'react-icons-kit/linea/basic_upload';
+import ClipLoader from 'react-spinners/ClipLoader';
+import BarLoader from 'react-spinners/BarLoader';
 
 class SignUp extends Component {
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (prevProps.authError !== this.props.authError) {
+  //     this.setState({ loading: false });
+  //   }
+  // }
+
+  static getDerivedStateFromProps(props, state) {
+    if (state.loading) return { loading: false };
+  }
+
   state = {
     email: '',
     password: '',
@@ -19,6 +32,7 @@ class SignUp extends Component {
     avatar: '',
     isUploading: false,
     progress: 0,
+    loading: false,
     avatarURL:
       'https://firebasestorage.googleapis.com/v0/b/abstract-lane-269917.appspot.com/o/images%2Fuser-icon-silhouette.jpg?alt=media&token=71e1f4d0-129b-4d59-a6be-ee6fd99e2a28'
   };
@@ -27,9 +41,9 @@ class SignUp extends Component {
       [e.target.id]: e.target.value
     });
   };
-  handleSubmit = e => {
+  handleSubmit = async e => {
     e.preventDefault();
-
+    this.setState({ loading: true });
     this.props.signUp(this.state);
   };
 
@@ -42,13 +56,16 @@ class SignUp extends Component {
     console.error(error);
   };
   handleUploadSuccess = filename => {
-    this.setState({ avatar: filename, progress: 100, isUploading: false });
+    this.setState({ avatar: filename, progress: 100 });
     firebase
       .storage()
       .ref('images')
       .child(filename)
       .getDownloadURL()
       .then(url => this.setState({ avatarURL: url }));
+    setTimeout(() => {
+      this.setState({ isUploading: false });
+    }, 1000);
   };
 
   render() {
@@ -119,46 +136,73 @@ class SignUp extends Component {
                   <input type="text" id="phone" onChange={this.handleChange} />
                 </div>
                 <div style={{ display: 'flex', flexDirection: 'column' }}>
-                  <img
-                    src={this.state.avatarURL}
-                    // htmlFor="image"
-                    style={{
-                      width: '90px',
-                      height: '90px',
+                  {this.state.isUploading ? (
+                    <ClipLoader
+                      size={120}
+                      //size={"150px"} this also works
+                      css={{ marginLeft: '20px' }}
+                      color={'#fbd800'}
+                      loading={this.state.isUploading}
+                    />
+                  ) : (
+                    <React.Fragment>
+                      <img
+                        src={this.state.avatarURL}
+                        // htmlFor="image"
+                        style={{
+                          width: '90px',
+                          height: '90px',
 
-                      margin: '0 auto'
-                    }}
-                    alt="userIcon"
-                  />
-
-                  {/* <input
-                    type="file"
-                    id="image"
-                    onChange={this.handleChange}
-                    style={{ width: '200px' }}
-                  /> */}
-                  <FileUploader
-                    accept="image/*"
-                    name="avatar"
-                    randomizeFilename
-                    storageRef={firebase.storage().ref('images')}
-                    onUploadStart={this.handleUploadStart}
-                    onUploadError={this.handleUploadError}
-                    onUploadSuccess={this.handleUploadSuccess}
-                    onProgress={this.handleProgress}
-                    style={{ width: '200px' }}
-                  />
+                          margin: '0 auto'
+                        }}
+                        alt="userIcon"
+                      />
+                      <label>
+                        <div
+                          style={{
+                            width: 34,
+                            height: 34,
+                            color: '#fbd800',
+                            margin: '2px auto'
+                          }}
+                        >
+                          <Icon size={'100%'} icon={basic_upload} />
+                        </div>
+                        <FileUploader
+                          accept="image/*"
+                          name="avatar"
+                          randomizeFilename
+                          storageRef={firebase.storage().ref('images')}
+                          onUploadStart={this.handleUploadStart}
+                          onUploadError={this.handleUploadError}
+                          onUploadSuccess={this.handleUploadSuccess}
+                          onProgress={this.handleProgress}
+                          style={{ width: '200px', display: 'none' }}
+                        />
+                      </label>
+                    </React.Fragment>
+                  )}
                 </div>
               </div>
             </div>
-            <div className="input-field">
-              <button className="btn mybtn1 transparent lighten-1 z-depth-0">
-                Sign Up
-              </button>
-              <div className="center red-text">
-                {authError ? <p>{authError}</p> : null}
+            {this.state.loading ? (
+              <BarLoader
+                size={150}
+                css={{ margin: '10px 155px' }}
+                //size={"150px"} this also works
+                color={'#fbd800'}
+                loading={this.state.loading}
+              />
+            ) : (
+              <div className="input-field">
+                <button className="btn mybtn1 transparent lighten-1 z-depth-0">
+                  Sign Up
+                </button>
+                <div className="center red-text">
+                  {authError ? <p>{authError}</p> : null}
+                </div>
               </div>
-            </div>
+            )}
           </form>
         </div>
       </div>
